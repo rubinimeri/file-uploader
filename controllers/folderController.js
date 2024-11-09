a8fc7804d73bb425eb61814e41977aed60ef0e8e
Rubin Imeri <rubinimeri6@gmail.com>
2024-11-09 17:16:33 +0100

    Add error handling for folderController

--- controllers/folderController.js
+++ controllers/folderController.js
@@ -1,8 +1,10 @@
 const { PrismaClient } = require('@prisma/client');
 const cloudinary = require('../utils/cloudinaryConfig');
+const CustomError = require('../utils/customError');
+const asyncHandler = require("express-async-handler");
 const prisma = new PrismaClient();
 
-async function folderGet(req, res) {
+const folderGet = asyncHandler(async (req, res) => {
     const { user } = req;
     const { folderId } = req.params;
 
@@ -30,22 +32,28 @@ async function folderGet(req, res) {
         name: rootFolder.name,
         parentId: rootFolder.parentId
     }]) {
-        if (!currentFolders[currentFolders.length-1].parentId) {
-            return currentFolders;
+        try {
+
+            if (!currentFolders[currentFolders.length - 1].parentId) {
+                return currentFolders;
+            }
+            const parentFolder = await prisma.folder.findFirst({
+                where: {id: currentFolders[currentFolders.length - 1].parentId},
+            })
+            currentFolders.push({id: parentFolder.id, name: parentFolder.name, parentId: parentFolder.parentId});
+            return await getFolders(currentFolders);
+
+        } catch (err) {
+            throw new CustomError(err.message, err.statusCode);
         }
-        const parentFolder = await prisma.folder.findFirst({
-            where: { id: currentFolders[currentFolders.length - 1].parentId },
-        })
-        currentFolders.push({ id: parentFolder.id, name: parentFolder.name, parentId: parentFolder.parentId });
-        return await getFolders(currentFolders);
     }
 
     const folders = (await getFolders()).reverse();
 
     res.render('folder', { folder: rootFolder, user: user, folders: folders });
-}
+})
 
-async function newFolderPost(req, res) {
+const newFolderPost = asyncHandler(async (req, res) => {
     const { folderId } = req.params;
     const { user } = req;
     const { folderName } = req.body;
@@ -57,10 +65,10 @@ async function newFolderPost(req, res) {
             parentId: folderId
         }
     })
-    res.redirect(`/${folderId}`);
-}
+    res.redirect(`/my-files/${folderId}`);
+})
 
-async function folderRenamePost(req, res) {
+const folderRenamePost = asyncHandler(async (req, res) => {
     const { folderId } = req.params;
     const { newName } = req.body;
 
@@ -72,68 +80,62 @@ async function folderRenamePost(req, res) {
             name: newName,
         },
     })
-    res.redirect(`/${folder.parentId}`);
-}
+    res.redirect(`/my-files/${folder.parentId}`);
+})
 
-async function folderDeletePost(req, res) {
+const folderDeletePost = asyncHandler(async (req, res) => {
     const { folderId } = req.params;
-    if (!folderId) {
-        return res.status(400).send('Folder id is required');
-    }
-    try {
-        const folder = await prisma.folder.findFirst({
-            where: {
-                id: folderId,
-            },
-            include: {folders: true, files: true}
-        })
 
-        // Recursively delete folders & files
-        async function deleteFolders(currentFolderId = folder.id) {
-            try {
-                // If there is no folder id, return
-                if (!currentFolderId)
-                    return;
+    const folder = await prisma.folder.findFirst({
+        where: {
+            id: folderId,
+        },
+        include: {folders: true, files: true}
+    })
 
-                const currentFolder = await prisma.folder.findFirst({
-                    where: {
-                        id: currentFolderId,
-                    },
-                    include: {folders: true, files: true}
+    // Recursively delete folders & files
+    async function deleteFolders(currentFolderId = folder.id) {
+        try {
+            // If there is no folder id, return
+            if (!currentFolderId)
+                return;
+
+            const currentFolder = await prisma.folder.findFirst({
+                where: {
+                    id: currentFolderId,
+                },
+                include: {folders: true, files: true}
+            })
+
+            // If currentFolder has files then delete them
+            if (currentFolder.files.length !== 0) {
+                currentFolder.files.forEach(async (file) => {
+                    // Delete from database
+                    await prisma.file.delete({ where: { id: file.id } });
+
+                    // Delete from cloudinary
+                    const { public_id } = await cloudinary.api.resource_by_asset_id(file.cloudinaryId);
+                    await cloudinary.uploader.destroy(public_id);
                 })
-
-                // If currentFolder has files then delete them
-                if (currentFolder.files.length !== 0) {
-                    currentFolder.files.forEach(async (file) => {
-                        // Delete from database
-                        await prisma.file.delete({ where: { id: file.id } });
-
-                        // Delete from cloudinary
-                        const { public_id } = await cloudinary.api.resource_by_asset_id(file.cloudinaryId);
-                        await cloudinary.uploader.destroy(public_id);
-                    })
-                }
-
-                // Delete currentFolder
-                await prisma.folder.delete({where: {id: currentFolder.id}});
-
-                // If current folder has folders, recursively delete them
-                if (currentFolder.folders.length !== 0) {
-                    currentFolder.folders.forEach(async (folder) => {
-                        await deleteFolders(folder.id)
-                    })
-                }
-            } catch (err) {
-                console.error(err);
             }
-        }
 
-        deleteFolders();
-        res.redirect(`/${folder.parentId}`);
-    } catch (err) {
-        res.status(500).send('Failed to delete folder')
+            // Delete currentFolder
+            await prisma.folder.delete({where: {id: currentFolder.id}});
+
+            // If current folder has folders, recursively delete them
+            if (currentFolder.folders.length !== 0) {
+                currentFolder.folders.forEach(async (folder) => {
+                    await deleteFolders(folder.id)
+                })
+            }
+        } catch (err) {
+            throw new CustomError(err.message, err.statusCode)
+        }
     }
-}
+
+    deleteFolders();
+    res.redirect(`/my-files/${folder.parentId}`);
+})
 
 module.exports = {
     folderGet,
