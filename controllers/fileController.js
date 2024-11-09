1f1a59624c42089ccaf963f4a67100794210d75e
Rubin Imeri <rubinimeri6@gmail.com>
2024-11-09 11:15:38 +0100

    Add download functionality for files

--- controllers/fileController.js
+++ controllers/fileController.js
@@ -1,5 +1,6 @@
 const upload = require('../middleware/multerConfig');
 const cloudinary = require('../utils/cloudinaryConfig');
+const axios = require('axios');
 const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 
@@ -16,7 +17,7 @@ const uploadFilePost = [
                 data: {
                     name: req.file.originalname,
                     size: req.file.size,
-                    type: result.resource_type,
+                    type: req.file.mimetype,
                     url: result.url,
                     cloudinaryId: result.asset_id,
                     userId: user.id,
@@ -28,6 +29,59 @@ const uploadFilePost = [
     }
 ]
 
+async function fileGet(req, res) {
+    const { user } = req;
+    const { fileId } = req.params;
+
+    if(!user) {
+        res.redirect(`/login`);
+    }
+
+    if(!fileId) {
+        res.status(204).end();
+    }
+
+    const file = await prisma.file.findFirst({
+        where: { id: fileId }
+    })
+    res.render('file', { file: file, user: user });
+}
+
+async function fileDownloadPost(req, res) {
+    const { fileId } = req.params;
+    if (!fileId) {
+        return res.status(400).send('File id is required');
+    }
+
+    try {
+        const file = await prisma.file.findFirst({
+            where: { id: fileId }
+        })
+
+        if(!file) {
+            return console.error('Error fetching from database')
+        }
+
+        const response = await axios({
+            url: file.url,
+            method: 'GET',
+            responseType: 'stream'
+        })
+
+        const { name } = file;
+
+        res.setHeader('Content-Disposition', `attachment; filename="${name}"`);
+        res.setHeader('Content-Type', response.headers['content-type'] || 'application/octet-stream');
+
+        response.data.pipe(res);
+
+    } catch (err) {
+        res.status(500).send('Error downloading file.');
+    }
+}
+
 module.exports = {
-    uploadFilePost
+    uploadFilePost,
+    fileGet,
+    fileDownloadPost
 }
--- routes/index.js
+++ routes/index.js
@@ -15,6 +15,8 @@ router.post('/login', authController.loginPost);
 router.post('/logout', authController.logoutPost);
 
 router.post('/upload/:folderId', fileController.uploadFilePost);
+router.get('/file/:fileId', fileController.fileGet);
+router.get('/download/:fileId', fileController.fileDownloadPost);
 
 router.post('/rename/:folderId', folderController.folderRenamePost);
 router.post('/delete/:folderId', folderController.folderDeletePost);
