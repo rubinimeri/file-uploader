17a0d226f5bc25810a7bf1290cd6a710d2b68fa2
Rubin Imeri <rubinimeri6@gmail.com>
2024-11-09 16:30:05 +0100

    Add global error handling & install express-async-handler

--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,20 @@ const fileRouter = require('./fileRouter');
 const authRouter = require('./authRouter');
 const folderRouter = require('./folderRouter');
 const indexController = require('../controllers/indexController');
+const globalErrorHandler = require('../controllers/errorController');
+const CustomError = require('../utils/customError');
 
 const router = new Router();
 
 router.get('/', indexController.homepageGet);
 router.use('/', authRouter);
 router.use('/file', fileRouter);
-router.use('/', folderRouter);
+router.use('/my-files', folderRouter);
+
+router.all('*', (req, res, next) => {
+    const err = new CustomError(`Can't find ${req.originalUrl} on the server!`, 404)
+    next(err)
+})
+router.use(globalErrorHandler)
 
 module.exports = router;
